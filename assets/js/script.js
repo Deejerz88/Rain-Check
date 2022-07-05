876a5dcde4474aac4a63835dd34467515d463a8a
Deejerz88 <djmcmillan93@gmail.com>
2022-07-05 06:14:54 -0400

    got animation timing down. Background changes with current weather, other fancy stuff.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const body = $("body");
 const search = $("#search");
 const today = $("#today");
 const weather = $("#weather");
@@ -5,6 +6,15 @@ const ui = $("#ui");
 let searchEl;
 
 const DateTime = luxon.DateTime;
+const background = {
+  clouds: "assets/images/clouds.jpg",
+  rain: "assets/images/raining.jpg",
+  clear: "assets/images/sunny.jpg",
+  thunderstorm: "assets/images/thunderstorm.jpg",
+  night: "assets/images/night.jpg",
+  sunrise: "assets/images/sunrise.jpg",
+  sunset: "assets/images/sunset.jpg",
+};
 
 let searchHistory = JSON.parse(localStorage.getItem("weatherHistory"));
 searchHistory = !!searchHistory ? searchHistory : {};
@@ -16,14 +26,13 @@ weather.hide();
 ui.width("375");
 
 const createSearch = () => {
-
-  //Search City
+  //Search Location
   const searchInput = $("<input/>");
   searchInput
     .attr({ id: "search-input", placeholder: "Search City", type: "text" })
     .addClass("form-control mb-3");
   const searchLabel = $("<label/>");
-  searchLabel.attr("for", "search-input").text("Search City").addClass("m-3");
+  searchLabel.attr("for", "search-input").text("Search Location").addClass("m-3");
   search.append(searchInput).append(searchLabel);
   searchEl = $("#search-input");
 
@@ -33,8 +42,10 @@ const createSearch = () => {
     .text("Get Weather")
     .click(getLocation)
     .attr("id", "searchBtn")
-    .addClass("btn btn-primary mb-3 align-self-end");
+    .addClass("btn btn-primary mb-1 align-self-end");
   search.append(searchBtn);
+  const hr = $('<hr>')
+  search.append(hr)
 
   showHistory();
 };
@@ -47,9 +58,10 @@ const showHistory = () => {
 
   historyEl.addClass("container");
   search.append(historyEl);
-  let maxHistory = searches.length >= 10 ? -10 : -searches.length
-  console.log(maxHistory)
-  searches = searches.slice(maxHistory)
+  const numShow = 14
+  let maxHistory = searches.length >= numShow ? -numShow : -searches.length;
+  console.log(maxHistory);
+  searches = searches.slice(maxHistory);
   console.log({ searches });
   searches.forEach((history) => {
     addToHistory(history);
@@ -60,17 +72,18 @@ const addToHistory = (history) => {
   const historyBtn = $("<button/>");
   const rowContainer = $("<div>");
   rowContainer.addClass("row");
-  historyBtn.text(history).click(getLocation).addClass("btn btn-info mb-3 col");
-  rowContainer.append(historyBtn);
+  historyBtn.text(history).click(getLocation).addClass("btn btn-info mb-3");
+  rowContainer.append(historyBtn.attr("id", "historyBtn"));
   historyEl.append(rowContainer);
 };
 
 //Get Search Coordinates
 const getLocation = (e) => {
+  weather.fadeOut();
   
   const tgt = $(e.target);
   console.log(tgt);
-  console.log({ searchEl });
+  console.log( 'searchEl val',searchEl.val() );
   const location =
     tgt[0].id === "searchBtn" ? searchEl.val() : tgt[0].textContent;
   console.log(location);
@@ -84,6 +97,8 @@ const getLocation = (e) => {
         searchHistory[location] = coord;
         localStorage.setItem("weatherHistory", JSON.stringify(searchHistory));
         if (tgt[0].id === "searchBtn") addToHistory(location);
+        ui.width("1500");
+        weather.fadeIn();
         getWeather(coord.lat, coord.lng, location);
       });
   });
@@ -91,7 +106,6 @@ const getLocation = (e) => {
 
 //Get Weather from Coordinates
 const getWeather = (lat, lon, loc) => {
-  
   console.log(`getting weather for ${lat},${lon}`);
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${config.OW_API}&units=imperial`;
   console.log(url);
@@ -100,9 +114,10 @@ const getWeather = (lat, lon, loc) => {
       res.json().then((data) => {
         console.log({ data });
         // weather.show()
-        weather.fadeOut('fast');
-
-        ui.width("375");
+        // $('#ui-container').css({ 'justify-content': 'start' })
+        
+        
+        // ui.width("375");
         displayWeather(data, loc, today);
       });
   });
@@ -135,41 +150,74 @@ function degToCompass(num) {
 //Contruct Weather Modules
 const displayWeather = (data, loc, today) => {
   // console.log(today);
+  ui.css('opacity', 0.93)
   
   today.empty();
   console.log({ loc });
-
-  
+  body.attr({'opacity':0})
 
   let id = today.attr("id");
+  const timezone = data.timezone;
   const current = id === "today" ? data.current : data;
-  let date = DateTime.fromSeconds(current.dt).toLocaleString(
-    DateTime.DATE_MED_WITH_WEEKDAY
-  );
-  let sep = "<br>--";
+  let currentDate = DateTime.fromSeconds(current.dt);
+  let fullTime = currentDate.setZone(timezone);
+  let time = currentDate.setZone(timezone).startOf("hour");
+  console.log({ time });
+  let date = currentDate.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
   let temp = current.temp.day;
+  const sunrise = DateTime.fromSeconds(current.sunrise)
+    .setZone(timezone)
+    .startOf("hour");
+  const sunset = DateTime.fromSeconds(current.sunset)
+    .setZone(timezone)
+    .startOf("hour");
+  console.log(
+    "sunrise,sunset,time",
+    sunrise.hour,
+    sunset.hour,
+    time.hour
+  );
 
   // today.addClass("container");
-  
 
   // today.hide()
   //City Title
   const city = $("<h2>");
+  let timeStr = ''
   if (id === "today") {
     // today.removeAttr('style')
+    let weatherType;
+    console.log(time.diff(sunrise,'hours').toObject().hours,'hours from sunrise')
+
+    if (Math.abs(time.hour-sunrise.hour) == 0) {
+      weatherType = "sunrise";
+    } else if (Math.abs(sunset.hour - time.hour) == 0) {
+      weatherType = "sunset";
+    } else if (sunset.hour < time.hour || time.hour < sunrise.hour) {
+      weatherType = "night";
+    } else {
+      weatherType = current.weather[0].main.toString().toLowerCase();
+    }
+    let bgSrc = background[weatherType];
+    body.css({ "background": `url(${bgSrc}) center` ,'opacity':1});
     city.text(`${loc} -- `);
     temp = current.temp;
-    sep = " | ";
+    let now = fullTime.toLocaleString(DateTime.TIME_SIMPLE);
+    timeStr = `<h5> @ ${now}</h5>`;
   } else {
-    date = date.substring(0, date.lastIndexOf(","))
+    date = date.substring(0, date.lastIndexOf(","));
   }
-
-  city.append(`<i>${date}</id>`);
+  
+  console.log({ timeStr });
+  city.append(`<i>${date}</i>${timeStr}`);
   // console.log(city.text())
 
   //Weather Icons
   const icon = current.weather[0].icon;
   const iconEl = $("<img>");
+  const desc = current.weather[0].description;
+  const descEl = $("<p>");
+  descEl.html(`<i>${desc}</i>`);
   iconEl.attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
 
   //temperature
@@ -178,20 +226,17 @@ const displayWeather = (data, loc, today) => {
   const feelsLike =
     id === "today" ? current.feels_like : current.feels_like.day;
   tempEl.html(
-    `<b>Temperature:</b> ${temp}&deg;F -- <i>Feels Like: ${feelsLike}&deg;F</i>`
+    `<b>Temperature:</b> ${temp}&deg;F <br>&ensp;-- <i>Feels Like: ${feelsLike}&deg;F</i>`
   );
 
   //wind
   const windEl = $("<p>");
   const windSpeed = current.wind_speed;
-  const windGust = current.wind_gust;
   const windDeg = current.wind_deg;
 
   const windDir = degToCompass(windDeg);
 
-  windEl.html(
-    `<b>Wind Speed:</b> ${windSpeed} MPH${sep}<b>Direction:</b> ${windDir}${sep}<b>Wind Gust:</b> ${windGust}`
-  );
+  windEl.html(`<b>Wind:</b> ${windSpeed} MPH <i>${windDir}</i>`);
 
   //humidity
   const humidityEl = $("<p>");
@@ -204,18 +249,19 @@ const displayWeather = (data, loc, today) => {
   const uviBtn = $("<button>");
   const uvLevels = ["success", "warning", "danger"];
   uvEl.html(`<b>UV Index:</b> `);
-  const uvLevel = uvLevels[Math.floor(uvIndex / 4)];
+  const uvLevel = uvLevels[Math.min(Math.floor(uvIndex / 4), 2)];
   uviBtn.text(uvIndex).addClass(`btn btn-${uvLevel}`);
 
   uvEl.append(uviBtn);
   today.append(city);
   today.append(iconEl);
+  today.append(descEl);
   today.append(tempEl);
   today.append(windEl);
   today.append(humidityEl);
   today.append(uvEl);
-  weather.fadeIn();
-  ui.width("1500");
+  
+  
   if (today.attr("id") === "today")
     today.animate(
       {
